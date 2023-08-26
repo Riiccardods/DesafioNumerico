c507df4d3f9ff55aebece16499d956b9770ce76c
Riiccardods <riicardoljn@icloud.com>
2023-08-26 19:36:23 -0300

    Atualização 01

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace JogoAdivinhacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao Jogo de Adivinhação!");
+
+            // Gera um número aleatório entre 1 e 100
+            Random random = new Random();
+            int numeroSecreto = random.Next(1, 101);
+
+            int tentativas = 0;
+            bool acertou = false;
+
+            while (!acertou)
+            {
+                Console.WriteLine("Escolha um número entre 1 e 100:");
+                string? entrada = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(entrada))
+                {
+                    Console.WriteLine("Por favor, insira um número válido.");
+                    continue;
+                }
+
+                int palpite;
+                if (!int.TryParse(entrada, out palpite) || palpite < 1 || palpite > 100)
+                {
+                    Console.WriteLine("Por favor, insira um número válido entre 1 e 100.");
+                    continue;
+                }
+
+                tentativas++;
+
+                if (palpite == numeroSecreto)
+                {
+                    acertou = true;
+                    Console.WriteLine($"Parabéns! Você acertou o número em {tentativas} tentativas.");
+                    Console.WriteLine("Pressione qualquer tecla para sair.");
+                    Console.ReadLine();
+                }
+                else if (palpite < numeroSecreto)
+                {
+                    Console.WriteLine("O número secreto é maior! Tente novamente.");
+                }
+                else
+                {
+                    Console.WriteLine("O número secreto é menor! Tente novamente.");
+                }
+            }
+        }
+    }
+}
