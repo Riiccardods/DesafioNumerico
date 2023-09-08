5a8d69956ad23a9aad7b2fe19ca5c5a24592c9ff
Riiccardods <riicardoljn@icloud.com>
2023-09-08 20:37:17 -0300

    Atualização

--- Program.cs
+++ Program.cs
@@ -6,9 +6,7 @@ namespace DesafioNumerico
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Bem-vindo ao Desafio Numerico!");
-
 
             // Gera um numero aleatório
 
